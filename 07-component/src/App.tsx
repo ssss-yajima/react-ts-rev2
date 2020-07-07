8f7b0f5cbd21ff7a51c2b77eb737dac73762f44f
y_megane <y.megane88@gmail.com>
2020-07-08 00:50:53 +0900

    7-2

--- 07-component/src/App.tsx
+++ 07-component/src/App.tsx
@@ -1,22 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import CharacterList, { Character } from './CharacterList';
+
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>Hello React!</p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+class App extends Component {
+  render() {
+    const characters: Character[] = [
+      {
+        id: 1,
+        name: '羽咲 綾乃',
+        age: 16,
+        height: 151,
+      },
+      {
+        id: 2,
+        name: '荒垣 なぎさ',
+        age: 18,
+        height: 174,
+      },
+      {
+        id: 3,
+        name: '泉 理子',
+        age: 18,
+      },
+    ];
 
+    return (
+      <div className="container">
+        <header>
+          <h1>はねバド！ キャラクター一覧</h1>
+        </header>
+        <CharacterList school="北小町高校" characters={characters} />
+      </div>
+    );
+  }
+}
 export default App;
