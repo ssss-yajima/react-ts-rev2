5685c961e3257264a6d70504942c12a993d27339
y_megane <y.megane88@gmail.com>
2020-07-11 02:20:17 +0900

    8-4 EffectHookでライフサイクルを扱う

--- 07-component/07-04-lifecycle/src/App.tsx
+++ 07-component/07-04-lifecycle/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { Button, Card, Icon, Statistic } from 'semantic-ui-react';
 
 import './App.css';
@@ -9,59 +9,41 @@ interface AppState {
   timeLeft: number;
 }
 
-class App extends Component<{}, AppState> {
-  timerId?: NodeJS.Timer;
+const App: FC = () => {
+  const [timeLeft, setTimeLeft] = useState(LIMIT);
 
-  constructor(props: {}) {
-    super(props);
-    this.state = { timeLeft: LIMIT };
-  }
-
-  reset = () => {
-    this.setState({ timeLeft: LIMIT });
+  const reset = () => {
+    setTimeLeft(LIMIT);
   };
 
-  tick = () => {
-    this.setState(prevState => ({ timeLeft: prevState.timeLeft - 1 }));
+  const tick = () => {
+    setTimeLeft(prevTime => (prevTime === 0 ? LIMIT : prevTime - 1));
   };
 
-  componentDidMount = () => {
-    this.timerId = setInterval(this.tick, 1000);
-  };
+  useEffect(() => {
+    const timerId = setInterval(tick, 1000);
 
-  componentDidUpdate = () => {
-    const { timeLeft } = this.state;
-    if (timeLeft === 0) {
-      this.reset();
-    }
-  };
+    return () => clearInterval(timerId);
+  }, []);
 
-  componentWillUnmount = () => {
-    clearInterval(this.timerId as NodeJS.Timer);
-  };
-
-  render() {
-    const { timeLeft } = this.state;
-
-    return (
-      <div className="container">
-        <header>
-          <h1>Timer</h1>
-        </header>
-        <Card>
-          <Statistic className="number-board">
-            <Statistic.Label>time</Statistic.Label>
-            <Statistic.Value>{timeLeft}</Statistic.Value>
-          </Statistic>
-          <Card.Content>
-            <Button color="red" fluid onClick={this.reset}>
-              <Icon name="redo" />
-              Reset
-            </Button>
-          </Card.Content>
-        </Card>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="container">
+      <header>
+        <h1>Timer</h1>
+      </header>
+      <Card>
+        <Statistic className="number-board">
+          <Statistic.Label>time</Statistic.Label>
+          <Statistic.Value>{timeLeft}</Statistic.Value>
+        </Statistic>
+        <Card.Content>
+          <Button color="red" fluid onClick={reset}>
+            <Icon name="redo" />
+            Reset
+          </Button>
+        </Card.Content>
+      </Card>
+    </div>
+  );
+};
 export default App;
