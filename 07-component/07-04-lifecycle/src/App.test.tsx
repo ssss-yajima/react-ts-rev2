5b5fbaaa8f8c7aff1d0b1402adb1c28360f77ff4
y_megane <y.megane88@gmail.com>
2020-07-14 01:36:42 +0900

    8-6 その他のHooks
    ComponentとContainerを分離。
    カスタムHooksを作成。

--- 07-component/07-04-lifecycle/src/App.test.tsx
+++ 07-component/07-04-lifecycle/src/App.test.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable jest/expect-expect */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import AppContainer from './containers/App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<AppContainer />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- 07-component/07-04-lifecycle/src/index.tsx
+++ 07-component/07-04-lifecycle/src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import './index.css';
 import 'semantic-ui-css/semantic.min.css';
+import AppContainer from './containers/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppContainer />
   </React.StrictMode>,
   document.getElementById('root'),
 );
